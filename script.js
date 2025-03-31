816a57f4076023a2c3f567a4cf5ca868ad9372f5
dev-sweet <sweetali0520@gmail.com>
2025-03-31 23:25:11 +0600

    search and filter

--- script.js
+++ script.js
@@ -1,32 +1,90 @@
 const booksContainer = document.getElementById("books");
 
-const fetchBooks = async () => {
+const fetchBooks = async (searchQuery = "", filter = "") => {
   try {
-    const res = await fetch("https://gutendex.com/books/");
-    const data = await res.json();
+    // at first show the loading element
+    booksContainer.innerHTML = "<h4>..Loading</h4>";
 
-    showBooks(data);
+    // fetch the data by searchquery
+    const res = await fetch(
+      `https://gutendex.com/books/?search=${encodeURIComponent(searchQuery)}`
+    );
+    const data = await res.json();
+    let books = data.results;
+
+    const bookFilter = document.getElementById("bookFilter");
+    const topics = [];
+
+    // get all topics in a single array
+    books.forEach((book) =>
+      book?.subjects?.forEach(
+        (subject) => !topics.includes(subject) && topics.push(subject)
+      )
+    );
+
+    // set topics in filter option
+    topics.forEach((topic) => {
+      const option = document.createElement("option");
+      option.value = topic;
+      option.innerText = topic;
+      bookFilter.appendChild(option);
+    });
+    if (filter) {
+      books = data.results.filter((book) => book.subjects.includes(filter));
+    }
+
+    // show the books
+    showBooks(books);
   } catch (err) {
     console.log(err);
   }
 };
 const showBooks = (books) => {
+  booksContainer.innerHTML = "";
   //   show book lists
 
-  books.results.forEach((book) => {
+  books.forEach((book) => {
     const bookCard = document.createElement("div");
     bookCard.classList.add("book-card");
-    console.log(book);
+
     const image = book.formats["image/jpeg"];
+    const author = book?.authors[0].name;
+    const genres = book?.subjects.join(",");
 
     bookCard.innerHTML = `
     <img class="book-img" src="${image}"/>
-    <h1 class="book-title">${book.title}</h1>
-    <p>author</p>
-    <p>${book.summaries.slice(0, 2)}</p>
+    <div class="book-info">
+
+    <p class="book-id">#${book.id}</p>
+    <h2 class="book-title">${book.title}</h2>
+    <p>By: ${author}</p>
+    <p class="genres">${genres}</p>
+    <button onclick="handleAddCart(${book.id})" class="cart-btn"> <i class="far fa-heart"></i></button>
+    </div>
     `;
 
     booksContainer.appendChild(bookCard);
   });
 };
 fetchBooks();
+
+const handleSearch = () => {
+  const searchText = document.getElementById("searchText").value;
+  fetchBooks(searchText);
+};
+
+const filterBooks = (e) => {
+  fetchBooks("", e.target.value);
+};
+
+// handle add to cart
+const handleAddCart = (id) => {
+  const carts = JSON.parse(localStorage.getItem("carts")) || [];
+  if (!carts.includes(id)) {
+    carts.push(id);
+    localStorage.setItem("carts", JSON.stringify(carts));
+    alert(`${id} is added to cart`);
+  } else {
+    alert("This book is already in cart!");
+  }
+};
